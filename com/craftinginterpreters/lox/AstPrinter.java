94a0b0be87b1681d720aa3a74c31366efe7e12c6
nunzioono <nunzioonorati@gmail.com>
2024-06-14 13:02:52 +0200

    Implemented logical operators for expressions

--- com/craftinginterpreters/lox/AstPrinter.java
+++ com/craftinginterpreters/lox/AstPrinter.java
@@ -1,6 +1,7 @@
 package com.craftinginterpreters.lox;
 
 import com.craftinginterpreters.lox.Expr.Assign;
+import com.craftinginterpreters.lox.Expr.Logical;
 import com.craftinginterpreters.lox.Expr.Variable;
 
 class AstPrinter implements Expr.Visitor<String> {
@@ -53,5 +54,11 @@ class AstPrinter implements Expr.Visitor<String> {
   public String visitVariableExpr(Variable expr) {
     // TODO Auto-generated method stub
     throw new UnsupportedOperationException("Unimplemented method 'visitVariableExpr'");
+  }
+
+  @Override
+  public String visitLogicalExpr(Logical expr) {
+    // TODO Auto-generated method stub
+    throw new UnsupportedOperationException("Unimplemented method 'visitLogicalExpr'");
   }
 }
--- com/craftinginterpreters/lox/Interpreter.java
+++ com/craftinginterpreters/lox/Interpreter.java
@@ -22,6 +22,19 @@ class Interpreter implements Expr.Visitor<Object>,
     }
 
     @Override
+    public Object visitLogicalExpr(Expr.Logical expr) {
+      Object left = evaluate(expr.left);
+  
+      if (expr.operator.type == TokenType.OR) {
+        if (isTruthy(left)) return left;
+      } else {
+        if (!isTruthy(left)) return left;
+      }
+  
+      return evaluate(expr.right);
+    }
+
+    @Override
     public Object visitUnaryExpr(Expr.Unary expr) {
       Object right = evaluate(expr.right);
   
@@ -86,6 +99,16 @@ class Interpreter implements Expr.Visitor<Object>,
       return null;
     }
     
+    @Override
+    public Void visitIfStmt(Stmt.If stmt) {
+        if (isTruthy(evaluate(stmt.condition))) {
+        execute(stmt.thenBranch);
+        } else if (stmt.elseBranch != null) {
+        execute(stmt.elseBranch);
+        }
+        return null;
+    }
+
     @Override
     public Void visitPrintStmt(Stmt.Print stmt) {
       Object value = evaluate(stmt.expression);
